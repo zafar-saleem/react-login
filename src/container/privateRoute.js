193a88c88970d94d4fdbd85e0f9839e98d1850d2
Zafar Saleem <zafarsaleem3@gmail.com>
2019-02-11 12:43:43 -0500

    TASK: Private routes for authenticated user implemented

---
+++ src/container/privateRoute.js
@@ -0,0 +1,19 @@
+import React from 'react';  
+import { Redirect, Route } from 'react-router-dom';
+import { checkCookie } from '../utils/cookies';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (  
+  <Route { ...rest } render={props => (
+    checkCookie() !== null ? (
+      <Component { ...props } />
+    ) : (
+      <Redirect to={{
+          pathname: '/',
+          state: { from: props.location }
+        }}
+      />
+    )
+  )} />
+);
+
+export default PrivateRoute;
